d1c7189c3e3d15c70ed756972106bcf567041d96
iTzMiNOS <amirerf1998@gmail.com>
2023-06-23 10:28:13 +0300

    modified:   src/app/app.component.html 	modified:   src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,18 +10,30 @@ import {FormGroup,FormControl} from '@angular/forms';
 })
 export class AppComponent {
 
-  furrm = new FormGroup({
+  form = new FormGroup({
     fullName: new FormControl('',[
       Validators.required,
       Validators.minLength(5),
       Validators.maxLength(15)
       ]),
-    emailAddress: new FormControl(),
+    emailAddress: new FormControl('',[
+      Validators.required,
+      Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")
+      ]),
     fullAddress: new FormControl()
   });
 
   get fullName(){
-    return this.furrm.get('fullName');
+    return this.form.get('fullName');
+  }
+  get emailAddress(){
+    return this.form.get('emailAddress');
+  }
+  get fullAddress(){
+    return this.form.get('fullAddress');
+  }
+  onSubmit(){
+    console.log(this.form.value);
   }
 }
 
