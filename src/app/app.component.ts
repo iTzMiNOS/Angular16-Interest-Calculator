5ef8b79858731cb481efd990518137430a9982d2
iTzMiNOS <amirerf1998@gmail.com>
2023-06-25 14:26:59 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import {FormArray, FormBuilder, Validators} from '@angular/forms';
-import {FormGroup,FormControl} from '@angular/forms';
-import { noSpace } from './Validators/nospace.validators';
 
 @Component({
   selector: 'app-root',
@@ -9,74 +7,28 @@ import { noSpace } from './Validators/nospace.validators';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   form:any;
-  constructor(fb: FormBuilder){
-    this.form = fb.group({
-      fullName: ['',[
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      deposit: [, [
         Validators.required,
-        Validators.minLength(5),
-        Validators.maxLength(15)
+        Validators.maxLength(8)
       ]],
-      emailAddress: ['',[
-        Validators.required,
-        noSpace.noSpaceValidations,
-        Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")
-      ]],
-      fullAddress: ['',Validators.required],
-      skills: fb.array([])
-    })
-  }
-  //form = new FormGroup({
-  //  fullName: new FormControl('',[
-  //    Validators.required,
-  //    Validators.minLength(5),
-  //    Validators.maxLength(15)
-  //    ]),
-  //  emailAddress: new FormControl('',[
-  //    Validators.required,
-  //    Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")
-  //    ]),
-  //  fullAddress: new FormControl(),
-  //  skills: new FormArray([])
-  //});
-
-  
-  get fc(){
-    return this.form.controls;
-  }
-  
-  onSubmit(){
-    console.log(this.form.value);
-  }
-  addSkills(skills: HTMLInputElement,$event?:KeyboardEvent){
-    if($event){
-    if(($event.keyCode == 188 || $event.keyCode == 32) && skills.value.length > 1){
-      if(skills.value.length > 2 && /^[a-z\d]+$/i.test(skills.value.slice(0, skills.value.length - 1))){
-      (this.fc.skills as FormArray).push(
-      new FormControl(skills.value.slice(0, skills.value.length - 1)));
-      skills.value = '';
-      console.log(this.form.value);
-    }else{
-      skills.value = '';
-    }}
-    else if($event.key == "Enter"){
-      if(skills.value.length > 1 && /^[a-z\d]+$/i.test(skills.value)){
-        (this.fc.skills as FormArray).push(
-        new FormControl(skills.value));
-        skills.value = '';
-        console.log(this.form.value);}
-        else{
-          skills.value = '';
-        }
+      duration: ['0', [
+        Validators.required
+      ]]
+    });
+    }
+    onKeyDown(event: KeyboardEvent) {
+      if (event.key === 'e') {
+        event.preventDefault();
       }
     }
-  }
-    riseError(){
-      return true;
+    calculatePower(base: number, exponent: number): number {
+      return Math.pow(base, exponent);
     }
-    removeSkills(index: number){
-      (this.fc.skills as FormArray).removeAt(index);
-      return true;
+    generateRange(start: number, end: number): number[] {
+      return Array.from({ length: end - start + 1 }, (_, i) => start + i);
     }
 }
-
